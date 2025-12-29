706b207a54973b7203395f2ca53aa89d14136792
Paulo Henrique Sanches <paulosanches2002@live.com>
2025-12-29 16:22:50 -0300

    t

--- src/presentation/routes/index.js
+++ src/presentation/routes/index.js
@@ -2,10 +2,12 @@ const createPropertyRoutes = require('./propertyRoutes');
 const createStoreSettingsRoutes = require('./storeSettingsRoutes');
 const createUserRoutes = require('./userRoutes');
 const createAuthRoutes = require('./authRoutes');
+const createUploadRoutes = require('./uploadRoutes');
 
 module.exports = {
     createPropertyRoutes,
     createStoreSettingsRoutes,
     createUserRoutes,
     createAuthRoutes,
+    createUploadRoutes
 };
